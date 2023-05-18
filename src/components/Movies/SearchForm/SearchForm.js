3f900c80890b5f9fd64725e04162ce0d03fc96e6
Timur Gain <timur.gain@gmail.com>
2023-05-18 21:46:55 +0400

    feat: mobile SearchForm

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -4,15 +4,19 @@ import searchIconPath from "../../../images/icon_search.svg";
 function SearchForm() {
   return (
     <form className="search" action="">
-      <img className="search__icon" src={searchIconPath} alt="Search icon" />
-      <input className="search__text" placeholder="Фильм" type="text" />
-      <button className="search__submit" />
+
+      <div className="search__field">
+        <img className="search__icon" src={searchIconPath} alt="Search icon" />
+        <input className="search__text" placeholder="Фильм" type="text" />
+        <button className="search__submit" />
+      </div>
 
       <label className="search__checkbox">
         <input className="search__checkbox_type_functional" type="checkbox" />
         <span className="search__checkbox_type_visible" />
         <p className="search__checkbox-label">Короткометражки</p>
       </label>
+      
     </form>
   );
 }
