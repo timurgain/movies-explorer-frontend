3a460b6485cbe72b5bf41ec7e6bb2ce72f035c6b
Timur Gain <timur.gain@gmail.com>
2023-05-25 22:24:03 +0400

    feat: Navigation component works on mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,45 @@
+import React from "react";
 import "./Header.css";
 import LogoPath from "../../images/logo_app.svg";
-import Navigation from "../Navigation/Navigation"
+import Navigation from "../Navigation/Navigation";
+
+const MOBILE_WIDTH_UP_TO = 1023;
 
 function Header() {
+
+  const [isMobile, setIsMobile] = React.useState(false);
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = React.useState(false)
+
+  React.useEffect(() => {
+    function handleResize() {
+      window.innerWidth <= MOBILE_WIDTH_UP_TO
+        ? setIsMobile(true)
+        : setIsMobile(false);
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  function handleClickMenuBtn() {
+    setIsMobileMenuOpened(true)
+  }
+
+  function handleClickMenuCloseBtn() {
+    setIsMobileMenuOpened(false)
+  }
+
   return (
     <header className="header">
       <img className="header__logo" src={LogoPath} alt="Logo Movie Explorer" />
-      <Navigation />
+
+      {isMobile
+        ? <button className="header__menu-btn" onClick={handleClickMenuBtn} />
+        : <Navigation isMobile={false}/>}
+
+      {isMobile && isMobileMenuOpened
+        && <Navigation isMobile={true} onClose={handleClickMenuCloseBtn}/>}
+
     </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,39 @@
 import "./Navigation.css";
 import iconProfilePath from "../../images/icon_profile.svg";
 
-function Navigation() {
-  return (
+function Navigation({ isMobile, ...props }) {
+  return isMobile ?
+  (
+    <div className="popup">
+      <nav className="nav">
+        <button className="nav__close-btn" onClick={props.onClose} />
+        <ul className="nav__menu">
+          <li className="nav__item">
+            <button className="nav__btn">Главная</button>
+          </li>
+
+          <li className="nav__item">
+            <button className="nav__btn">Фильмы</button>
+          </li>
+
+          <li className="nav__item">
+            <button className="nav__btn">Сохранённые фильмы</button>
+          </li>
+
+          <li className="nav__item nav__item_type_profile">
+            <button className="nav__btn nav__btn_type_profile">
+              <img src={iconProfilePath} alt="Profile icon" />
+              Аккаунт
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+  ) : (
+
     <nav className="nav">
       <ul className="nav__menu">
-
         <li className="nav__item">
           <button className="nav__btn">Фильмы</button>
         </li>
@@ -20,9 +48,9 @@ function Navigation() {
             Аккаунт
           </button>
         </li>
-
       </ul>
     </nav>
+
   );
 }
 
