33ac24be2335132ca0573b9afd2858d359c26a57
Timur Gain <timur.gain@gmail.com>
2023-05-30 00:32:54 +0400

    feat: Promo component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,15 @@ import './Main.css';
 import Header from '../Header/Header';
 import Promo from './Promo/Promo';
 
+import Footer from '../Footer/Footer';
+
 
 function Main() {
   return (
     <>
-      <Header type={'promo'}/>
+      <Header type={'promo'} />
       <Promo />
+      <Footer />
     </>
   )
 }
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,8 +1,14 @@
 import './Promo.css';
+import promoImagePath from '../../../images/promo_image.svg'
 
 function Promo() {
   return (
-    <p>Promo</p>
+    <div className='promo'>
+      <div className='promo__container'>
+        <h1 className='promo__header'>Учебный проект студента факультета Веб-разработки.</h1>
+        <img className='promo__image' src={promoImagePath} alt="Scribble" />
+      </div>
+    </div>
   )
 }
 
